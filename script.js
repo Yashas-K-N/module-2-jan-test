15b2536cc602e96192b4322724c5416c2111ac57
yashas k n <yashasknyashaskn229@gmail.com>
2024-02-04 19:04:47 +0530

    made another change for

--- script.js
+++ script.js
@@ -14,4 +14,15 @@ inputbox.addEventListener("keyup", function(event){
     list.appendChild(listItem);
  }
 
+
+ function Add(){
+   if (inputbox.value === ''){
+     alert("you must write something!");
+ }
+   else {
+       let li = document.createElement("li");
+       li.innerHTML = inputbox.value;
+       list.appendChild(li);
+  }
+}
  
